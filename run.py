6e28bae6c6714bb922b232dca73f4d4a1f3c5bf2
ankitsree <33896808+ankitsree@users.noreply.github.com>
2021-03-16 19:16:47 +0000

    Update run.py

--- run.py
+++ run.py
@@ -1,5 +1,5 @@
 from flask_program import app,db
 
-if __name__ == '__main__':
+if __name__ == '__main__':#if this is the file is the main file then run the code below
     db.create_all()
     app.run(debug=True)
